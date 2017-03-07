6b6fea57cbee7cb782f3b0f372319831469fa2f1
Dan Martinez <danmartinez78@gmail.com>
2017-03-07 01:44:28 -0600

    class created and color/gradient thresholding done

---
+++ lanes.py
@@ -0,0 +1,132 @@
+import numpy as np
+import cv2
+import glob
+import matplotlib.pyplot as plt
+
+class lanes:
+    def __init__(self):
+        self.ret = None
+        self.mtx = None
+        self.dist = None
+
+    def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
+        # Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        # Apply x or y gradient with the OpenCV Sobel() function
+        # and take the absolute value
+        if orient == 'x':
+            abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
+        if orient == 'y':
+            abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1))
+        # Rescale back to 8 bit integer
+        scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
+        # Create a copy and apply the threshold
+        binary_output = np.zeros_like(scaled_sobel)
+        # Here I'm using inclusive (>=, <=) thresholds, but exclusive is ok too
+        binary_output[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+
+        # Return the result
+        return binary_output
+
+    def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+        # Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        # Take both Sobel x and y gradients
+        sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+        sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+        # Calculate the gradient magnitude
+        gradmag = np.sqrt(sobelx ** 2 + sobely ** 2)
+        # Rescale to 8 bit
+        scale_factor = np.max(gradmag) / 255
+        gradmag = (gradmag / scale_factor).astype(np.uint8)
+        # Create a binary image of ones where threshold is met, zeros otherwise
+        binary_output = np.zeros_like(gradmag)
+        binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
+
+        # Return the binary image
+        return binary_output
+
+    def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi / 2)):
+        # Grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        # Calculate the x and y gradients
+        sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+        sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+        # Take the absolute value of the gradient direction,
+        # apply a threshold, and create a binary image result
+        absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
+        binary_output = np.zeros_like(absgraddir)
+        binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
+
+        # Return the binary image
+        return binary_output
+
+        # Return the binary image
+        return binary_output
+
+    def gradient_thresh(self, gradx, grady, mag_binary, dir_binary, debug = False):
+        combined = np.zeros_like(dir_binary)
+        combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+        return combined
+
+    def color_thresh (self, image, s_thresh_min = 170, s_thresh_max = 255, debug = False):
+        hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        s_channel = hls[:, :, 2]
+        s_binary = np.zeros_like(s_channel)
+        s_binary[(s_channel >= s_thresh_min) & (s_channel <= s_thresh_max)] = 1
+
+        # Return the binary image
+        return s_binary
+
+    def full_thresh(self, gradient_thresh, color_thresh, debug = False):
+        combined_binary = np.zeros_like(gradient_thresh)
+        combined_binary[(color_thresh == 1) | (gradient_thresh == 1)] = 1
+
+        return combined_binary
+
+    def get_calibration(self, path):
+        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+        objp = np.zeros((6 * 9, 3), np.float32)
+        objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+
+        # Arrays to store object points and image points from all the images.
+        objpoints = []  # 3d points in real world space
+        imgpoints = []  # 2d points in image plane.
+
+        # Make a list of calibration images
+        # images = glob.glob('../camera_cal/calibration*.jpg')
+        images = glob.glob(path)
+
+        # Step through the list and search for chessboard corners
+        for fname in images:
+            img = cv2.imread(fname)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            # Find the chessboard corners
+            ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
+
+            # If found, add object points, image points
+            if ret == True:
+                objpoints.append(objp)
+                imgpoints.append(corners)
+
+                # Draw and display the corners
+                img = cv2.drawChessboardCorners(img, (9, 6), corners, ret)
+                cv2.imshow('img', img)
+                cv2.waitKey(500)
+
+        cv2.destroyAllWindows()
+        self.ret, self.mtx, self.dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    def undistort_images(self, image):
+        # if self.ret:
+        return cv2.undistort(image, self.mtx, self.dist, None, self.mtx)
+
+    def perspective_transform(self, image):
+
+
+
+    def process_frame(self, frame):
+        None
+
+
